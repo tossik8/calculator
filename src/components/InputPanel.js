56164ecaa7e0fb230d66f45c2aeb089eb9e4ef47
user <toropov.nikita2004@gmail.com>
2022-11-14 10:00:40 +0100

    Filter input

--- src/components/InputPanel.js
+++ src/components/InputPanel.js
@@ -7,21 +7,26 @@ const InputPanel = () => {
     const inputValue = useSelector(state => state.inputReducer.value);
 
     const handleChange = (event) =>{
-        dispatch(inputActions.handleInput(event.target.value));
         let content = event.target.value;
         let lastCharacter = content.charAt(content.length-1);
-        if(content.length === 22 && !isNaN(lastCharacter)){
-            dispatch(inputActions.handleInput("Digimit Limit Met"));
-            document.getElementById("input").disabled = true;
-            setTimeout(() => {
-                document.getElementById("input").value = content.substring(0, 21);
-                document.getElementById("input").focus();
-            }, 1000);
-            document.getElementById("input").disabled = false;
+        if((!isNaN(content) || lastCharacter === "*"|| lastCharacter === "/ "|| lastCharacter === "+"|| lastCharacter === "-"|| lastCharacter === ".") && lastCharacter!==" "){
+            dispatch(inputActions.handleInput(content));
+            if(content.length === 22 && !isNaN(lastCharacter)){
+                dispatch(inputActions.handleInput("Digimit Limit Met"));
+                document.getElementById("input").disabled = true;
+                setTimeout(() => {
+                    document.getElementById("input").value = content.substring(0, 21);
+                    document.getElementById("input").focus();
+                }, 1000);
+                document.getElementById("input").disabled = false;
+            }
+            else if(!isNaN(lastCharacter)){
+                dispatch(displayActions.handleInput(content));
+            }
         }
-        else if(!isNaN(lastCharacter)){
-            dispatch(displayActions.handleInput(content));
-        }
+
+
+
     }
 
     return (
